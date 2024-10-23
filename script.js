d4e70cb1d470c1a2c65bda1dbc729f9da75ba9ed
= <=>
2024-10-23 19:49:30 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//
+//document.getElementById("loginForm")?.addEventListener("submit", function (e) {
+// e.preventDefault();
+//const username = document.getElementById("username").value;
+//const password = document.getElementById("password").value;
+
+// Simulasi login sederhana
+//if (username === "user" && password === "password") {
+// alert("Login berhasil!");
+//window.location.href = "user-dashboard.html";
+//} else {
+//alert("Login gagal, coba lagi!");
+//}
+//});
+
+//document
+//.getElementById("registerForm")
+//?.addEventListener("submit", function (e) {
+//e.preventDefault();
+//const username = document.getElementById("username").value;
+//const email = document.getElementById("email").value;
+//const password = document.getElementById("password").value;
+
+// Simulasi register sederhana
+//    alert(`Registrasi berhasil untuk ${username}`);
+//  window.location.href = "login.html";
+//});
